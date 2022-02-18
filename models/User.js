6d077808c4f8e73be4589a4b1543ba3b559ad543
AKim47 <abel.kim.22@gmail.com>
2022-02-17 22:49:29 -0600

    created folders

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-zA-Z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-z\.]{2,6})$/
+                }
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                
+            }
+        ]
+
+    }
+)
+
+module.exports = User;
