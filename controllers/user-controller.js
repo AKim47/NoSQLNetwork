ba9f6e094c794bef966e86e22cf55f4f3cba0718
AKim47 <abel.kim.22@gmail.com>
2022-02-21 21:46:49 -0600

    add/delete friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,6 +8,10 @@ const userController = {
                 path: 'thoughts',
                 select: '-__v'
             })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             .sort({ _id: -1 })
             .then(dbUserData => res.json(dbUserData))
@@ -20,6 +24,16 @@ const userController = {
     // get one user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            },
+            {
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbUserData => {
                 // If no user is found, send 404
                 if (!dbUserData) {
@@ -65,6 +79,28 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(400).json(err));
+    },
+
+    // add friend by id
+    addFriend ({ params }, res) {
+        return User.findOneAndUpdate(
+            { _id: params.userId},
+            { $push: { friends: params.friendId}},
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // delete friend by id
+    deleteFriend ({ params }, res) {
+        return User.findOneAndUpdate(
+            { _id: params.userId},
+            { $pull: { friends: params.friendId}},
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
     }
 }
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,8 +6,8 @@ const {
     createUser,
     updateUser,
     deleteUser,
-    // addFriend,
-    // deleteFriend
+    addFriend,
+    deleteFriend
 } = require('../../controllers/user-controller');
 
 // Set up GET all and POST at /api/users
@@ -23,9 +23,9 @@ router
     .put(updateUser)
     .delete(deleteUser);
 
-// router
-//     .route('/:userId/friends/friendId')
-//     .post(addFriend)
-//     .delete(deleteFriend)
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
 
 module.exports = router;
